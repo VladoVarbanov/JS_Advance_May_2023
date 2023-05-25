c85bb74f775287a21d768c82465f3bfcda360c6b
VladoVarbanov <116281666+VladoVarbanov@users.noreply.github.com>
2023-05-25 18:04:32 +0300

    new tasks

--- DOM Manipulation and Events - Lab/01. List of Items_Ресурси/05. Highlight-Active/app.js
+++ DOM Manipulation and Events - Lab/01. List of Items_Ресурси/05. Highlight-Active/app.js
@@ -1,3 +1,20 @@
 function focused() {
-    console.log('TODO:...');
+  let input = document.querySelectorAll("input");
+  let inputArr = Array.from(input);
+
+  inputArr.forEach((x) => {
+    x.addEventListener("focus", focusHandler);
+    x.addEventListener("blur", blurHandler);
+  });
+
+  function focusHandler(e) {
+    let element = e.target;
+    let div = element.parentElement;
+    div.classList.add("focused");
+  }
+  function blurHandler(e) {
+    let element = e.target;
+    let div = element.parentElement;
+    div.classList.remove("focused");
+  }
 }
--- DOM Manipulation and Events - Lab/01. List of Items_Ресурси/07. Shopping-Cart/solution.js
+++ DOM Manipulation and Events - Lab/01. List of Items_Ресурси/07. Shopping-Cart/solution.js
@@ -1,3 +1,34 @@
 function solve() {
-   //TODO...
+  let addButtons = document.querySelectorAll(".add-product");
+  let addButtonsArr = Array.from(addButtons);
+  let textArea = document.querySelector("textarea");
+  let totalSum = 0;
+  let products = {};
+  let checkoutBtn = document.querySelector(".checkout");
+
+  addButtonsArr.forEach((x) => x.addEventListener("click", addHandler));
+  checkoutBtn.addEventListener("click", checkoutHandler);
+
+  function checkoutHandler(e) {
+    addButtonsArr.forEach((x) => (x.disabled = true));
+    let productNames = Object.keys(products).join(", ");
+    textArea.textContent += `You bought ${productNames} for ${totalSum.toFixed(
+      2
+    )}.`;
+  }
+
+  function addHandler(e) {
+    let element = e.target;
+    let productDiv = element.parentElement.parentElement;
+
+    let title = productDiv.querySelector(".product-title");
+    let priceDiv = productDiv.querySelector(".product-line-price");
+    let name = title.textContent;
+    let price = Number(priceDiv.textContent);
+    totalSum += price;
+    products[name] = true;
+    textArea.textContent += `Added ${name} for ${price.toFixed(
+      2
+    )} to the cart.\n`;
+  }
 }
