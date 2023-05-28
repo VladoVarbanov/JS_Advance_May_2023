5caadd27693d9ca28cd1a3369c74f227551825a4
VladoVarbanov <116281666+VladoVarbanov@users.noreply.github.com>
2023-05-28 20:52:35 +0300

    new tasks

--- DOM Introduction - Exercise/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/06. Format the Text/solution.js
+++ DOM Introduction - Exercise/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/06. Format the Text/solution.js
@@ -1,3 +1,52 @@
+// function solve() {
+//   const textArea = document.getElementById("input").value;
+//   let output = document.getElementById("output");
+//   const regex = /[A-Za-z]*[0-9]*[^.!?]*[.!?]/g;
+//   let textArr = textArea.match(regex);
+
+//   for (let i = 0; i < textArr.length; i += 3) {
+//     let p = document.createElement("p");
+//     for (let j = 0; j < 3; j++) {
+//       if (j >= textArr.length) {
+//         break;
+//       }
+//       p.textContent = p.textContent + textArr[i + j];
+//     }
+//     output.appendChild(p);
+//   }
+// }
+
+// function solve() {
+//   let getInputField = Array.from(
+//     document
+//       .querySelector("#input")
+//       .value.split(".")
+//       .filter((x) => x)
+//   );
+//   let getOutputField = document.querySelector("#output");
+//   let deepCopy = JSON.parse(JSON.stringify(getInputField));
+//   let sentenceCounter = 0;
+//   let node = document.createTextNode("");
+
+//   for (const sentence of getInputField) {
+//     node.appendData(`${sentence}`);
+//     sentenceCounter++;
+
+//     if (sentenceCounter === 3 || deepCopy.length === 1) {
+//       let paragraph = document.createElement("p");
+//     }
+//   }
+// }
+
 function solve() {
-  //TODO
+  let text = document.getElementById("input").value;
+  let arr = text.split(".").filter((x) => x !== "");
+  let result = document.getElementById("output");
+
+  while (arr.length > 0) {
+    let textArr = arr.splice(0, 3).join(".") + ".";
+    let p = document.createElement("p");
+    p.textContent = textArr;
+    result.appendChild(p);
+  }
 }
--- DOM Introduction - Exercise/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/08. Generate Report/report.js
+++ DOM Introduction - Exercise/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/08. Generate Report/report.js
@@ -1,3 +1,9 @@
 function generateReport() {
-    //TODO
+  let firstRow = Array.from(document.querySelectorAll("thead > tr th")).filter(
+    (x) => x.querySelector('input[type="checkbox"]').checked
+  );
+  let allTrPeople = Array.from(document.querySelectorAll("tbody tr"));
+  console.log(allTrPeople[0].querySelector("td").textContent);
+  let allPeople = [];
+  let People = {};
 }
