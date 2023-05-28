633ab49ef857226ca066348b4e437eea966cefcc
VladoVarbanov <116281666+VladoVarbanov@users.noreply.github.com>
2023-05-28 21:42:07 +0300

    new tasks

--- DOM Introduction - Exercise/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/08. Generate Report/report.js
+++ DOM Introduction - Exercise/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/08. Generate Report/report.js
@@ -3,7 +3,39 @@ function generateReport() {
     (x) => x.querySelector('input[type="checkbox"]').checked
   );
   let allTrPeople = Array.from(document.querySelectorAll("tbody tr"));
-  console.log(allTrPeople[0].querySelector("td").textContent);
+  let output = document.getElementById("output");
   let allPeople = [];
-  let People = {};
+  output.textContent = "";
+
+  for (let i = 0; i < allTrPeople.length; i++) {
+    const element = allTrPeople[i];
+    let person = {};
+
+    for (const th of firstRow) {
+      if (th.textContent === "Employee ") {
+        person["employee"] = element.querySelectorAll("td")[0].textContent;
+      }
+      if (th.textContent === "Department ") {
+        person.department = element.querySelectorAll("td")[1].textContent;
+      }
+      if (th.textContent === "Status ") {
+        person.status = element.querySelectorAll("td")[2].textContent;
+      }
+      if (th.textContent === "Date Hired ") {
+        person.dateHired = element.querySelectorAll("td")[3].textContent;
+      }
+      if (th.textContent === "Benefits ") {
+        person.benefits = element.querySelectorAll("td")[4].textContent;
+      }
+      if (th.textContent === "Compensation ") {
+        person.salary = element.querySelectorAll("td")[5].textContent;
+      }
+      if (th.textContent === "Rating ") {
+        person.rating = element.querySelectorAll("td")[6].textContent;
+      }
+    }
+    allPeople.push(person);
+  }
+  output.textContent = JSON.stringify(allPeople);
+  //   output.textContent = allPeople;
 }
